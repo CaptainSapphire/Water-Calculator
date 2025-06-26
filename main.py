d9120ee00b13ea7f0147c16565a638a3cb273365
misty_stars <122335357+CaptainSapphire@users.noreply.github.com>
2025-06-26 11:47:11 -0700

    Update main.py

--- main.py
+++ main.py
@@ -3,13 +3,13 @@
 # Make it imperial versus metric system
 # Maybe make it in metric, and then make a conversion function?
 
-
+# water bottle amount
+water_bottle = input("What is the total ounces your water bottle can hold?")
 
 # The U.S. National Academies of Sciences, Engineering, and Medicine determined that an adequate daily fluid intake is: About 15.5 cups (3.7 liters) of fluids a day for men. About 11.5 cups (2.7 liters) of fluids a day for women.
 gender = input("What is your biological sex? (F for female, M for male)")
-# This will be in liters?
-total_needed = 0
 
+total_needed = 0
 if (gender = "F") {
   total_needed = 2.7;
 }
