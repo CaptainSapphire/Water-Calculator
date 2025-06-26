b04b776ab2e6cf5b9010f37b5e1d4eeb59bcb72c
misty_stars <122335357+CaptainSapphire@users.noreply.github.com>
2025-06-26 14:29:36 -0700

    Update main.py

--- main.py
+++ main.py
@@ -4,8 +4,8 @@
 # Maybe make it in metric, and then make a conversion function?
 # L, mL = Metric, ounces = imperial
 
+####
 system = input("Are you in the imperial or metric system? (I=imperial, M= Metric)")
-
 if (system = "I") {
   print("imperial");
   unit = "ounces";
@@ -16,7 +16,7 @@ elif (system = "M") {
 }
 else {
   # Make this loop the system input
-  print("I'm sorry, I don't understand. I unfortunately cannot run this program without your unit of measurement.")
+  print("I'm sorry, I don't understand. I unfortunately cannot run this program without your measurement system.")
 }
 
 # water bottle amount
@@ -24,15 +24,28 @@ else {
 water_bottle = input("What is the total amount in " + unit + " your water bottle can hold?")
 
 # The U.S. National Academies of Sciences, Engineering, and Medicine determined that an adequate daily fluid intake is: About 15.5 cups (3.7 liters) of fluids a day for men. About 11.5 cups (2.7 liters) of fluids a day for women.
+# gender is a shorter term than biological sex so thats why the variable is like that
 gender = input("What is your biological sex? (F for female, M for male)")
 
 # How much water does a person need in total?
+# if sex  is girl
 total_needed = 0
-if (gender = "F") {
-  total_needed = 2.7;
+if (gender == "F") {
+  if (system == "M") {
+      total_needed = 0;
+  }
+  elif (system == "I") {
+        total_needed = 0;
+    }
 }
-if (gender = "M" {
- total_needed = 3.7
+# if sex is boy
+if (gender == "M" {
+  if (system == "M") {
+        total_needed = 0;
+    }
+  elif (system == "I") {
+        total_needed = 0;
+    }
 }
 
 # How many bottles of water are they going to drink
