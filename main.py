584d7c1c949a227a5106e32135183bdea2948441
misty_stars <122335357+CaptainSapphire@users.noreply.github.com>
2025-06-26 11:41:41 -0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+# Explain to user how to find the ounces on the water bottle/cup they drink, maybe make an algorithm based on the name that finds it for them
+
+# Make it imperial versus metric system
+# Maybe make it in metric, and then make a conversion function?
+
+
+
+# The U.S. National Academies of Sciences, Engineering, and Medicine determined that an adequate daily fluid intake is: About 15.5 cups (3.7 liters) of fluids a day for men. About 11.5 cups (2.7 liters) of fluids a day for women.
+gender = input("What is your biological sex? (F for female, M for male)")
+# This will be in liters?
+total_needed = 0
+
+if (gender = "F") {
+  total_needed = 2.7;
+}
+if (gender = "M" {
+ total_needed = 3.7
+}
